89c44af50e2019c7eaffd67cadd33e4b6293bb91
MatheusHm1 <matheushm182@gmail.com>
2026-03-12 02:17:34 -0300

    first commit

---
+++ CadastroClientes.dpr
@@ -0,0 +1,14 @@
+program CadastroClientes;
+
+uses
+  Vcl.Forms,
+  Cadastro in 'Cadastro.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
